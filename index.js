7cd3881785b6b47121cf7d6b09158955bf168a7e
trteel1 <tateelamino@gmail.com>
2022-11-05 16:34:00 +0300

    ass. com .

---
+++ index.js
@@ -0,0 +1,29 @@
+function add (a,b){
+return a+b
+}
+console.log(add(1000,41))
+function subtract(a,b) {
+return a-b
+}
+consol.log(subtract(700,4))
+function multiply(a,b) {
+return a*b
+}
+console.log(multiply(9,6758))
+function divide(a,b) {
+return a/b
+}
+console.log(divide(0.4536082474226804,1))
+function increment(n){
+    return n+1
+} 
+function decrement(n){
+   return n-1
+}
+
+function makeInt(n) {
+    return parseInt (n , 10)
+}
+function preserveDecimal(n) {
+    return parseFloat (n)
+}
